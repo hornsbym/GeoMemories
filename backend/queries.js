8aff7fb4ec86fd1b237b26f3d485db5de418cb0c
Mitch Hornsby <hornsbym19@mail.wlu.edu>
2019-05-19 15:39:51 -0400

    Began implementing query to add new memories to DB

--- backend/queries.js
+++ backend/queries.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 })
 
 const getUser = (request, response) => {
-    uname = request.query.username;
+    var uname = request.query.username;
     pool.query(`SELECT * FROM users WHERE username='${uname}'`, (error, results) => {
         if (error) {
             throw error;
@@ -18,14 +18,30 @@ const getUser = (request, response) => {
     })
 }
 
-// const getMemoriesForUser = (request, response) => 
 const getMemoriesForUser = (request, response) => {
-    uname = request.query.username;
+    var uname = request.query.username;
     pool.query(`SELECT lat, long, title, descr FROM users, memory_info WHERE username='${uname}'`, (error, results) => {
         if (error) {
             throw error;
         } else {
             response.status(200).json(results.rows)
+        }
+    })
+}
+
+const addMemoryForUser = (request, response) => {
+    var uname = request.query.username;
+    var title = request.query.title;
+    var lat  = request.query.latitude;
+    var long = request.query.longitude;
+    var desc = request.query.description;
+
+    pool.query(`INSERT INTO memory_info (lat, long, title, descr, user_id) 
+    VALUES (${lat}, ${long}, '${title}', '${desc}', (SELECT user_id FROM users WHERE username='${user_id}'))`, (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            response.status(200)
         }
     })
 }
--- frontend/mapFunctions.js
+++ frontend/mapFunctions.js
@@ -1,27 +1,72 @@
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 37.789148, lng: -79.441281 },
-    zoom: 5,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    minZoom: 3,
-    maxZoom: 10
-  });
+  var geojson = {
+    "type": "FeatureCollection",
+    "features": []
+  }
 
-  map.data.addListener('click', function (event) {
-    var title = event.feature.getProperty("title");
-    var content = event.feature.getProperty("content");
-    var infowindow = new google.maps.InfoWindow();
-    infowindow.setContent(title + content);
-    infowindow.setPosition(event.feature.getGeometry().get());
-    infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
-    infowindow.open(map);
-  });
+  // URL to send the GET request to:
+  var url = "http://cf6cd891.ngrok.io/getMemoriesForUser?username=hornsbym"
 
-  map.data.loadGeoJson("GeoJson.json");
+  // Sends a GET fetch request to the appropriate endpoint:
+  fetch(url).then(
+    (raw_data) => {
+      // Turns the database information into a JSON object:
+      raw_data.json().then(
+        // Adds the DB info to the GeoJSON object as a new feature:
+        (json_data) => {
+          // Loops over all data gathered from DB:
+          for (var i = 0; i < json_data.length; i++){
+            var geo_data = json_data[i];
 
-  map.data.setStyle(function (feature) {
-    return { icon: feature.getProperty('icon') };
-  });
+            // Dissect the JSON object:
+            var lat = geo_data.lat;
+            var long = geo_data.long;
+            var title = geo_data.title;
+            var desc = geo_data.descr;
+  
+            // Adds the new information to a new feature:
+            var feature = {
+              "type" : "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [ long , lat ]
+              },
+              "properties": {
+                "title": title,
+                "content": desc,
+                "icon": "http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png"
+              }
+            }
+  
+            // Puts the new feature in the GeoJSON object:
+            geojson.features.push(feature);
+          }
+
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 37.789148, lng: -79.441281 },
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            minZoom: 3,
+            maxZoom: 10
+          });
+
+          map.data.addListener('click', function (event) {
+            var title = event.feature.getProperty("title");
+            var content = event.feature.getProperty("content");
+            var infowindow = new google.maps.InfoWindow();
+            infowindow.setContent(title + content);
+            infowindow.setPosition(event.feature.getGeometry().get());
+            infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
+            infowindow.open(map);
+          });
+
+          map.data.addGeoJson(geojson);
+
+          map.data.setStyle(function (feature) {
+            return { icon: feature.getProperty('icon') };
+          });
+        })
+    });
 }
